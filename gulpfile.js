055adba22669a66df51c6787bdf71e75eb16bade
MarcioAlex-x <marcioalex586@gmail.com>
2024-04-17 15:45:37 -0300

    função default criada

--- gulpfile.js
+++ gulpfile.js
@@ -16,11 +16,17 @@ const compStyle = () =>{
 }
 
 const compImg = () => {
-    return gulp.src('./source/imagens/*.jpg')
+    return gulp.src('./source/imagens/*')
         .pipe(imagemin())
         .pipe(gulp.dest('./build/imagens'))
 }
 
-exports.javascript = compJS
-exports.estilos = compStyle
-exports.imagens = compImg
+exports.default = function(){
+    gulp.watch('./source/js/*.js', {ignoreInitial: false}, gulp.series(compJS))
+    gulp.watch('./source/styles/*.scss', {ignoreInitial: false}, gulp.series(compStyle))
+    gulp.watch('./source/imagens/*', {ignoreInitial: false}, gulp.series(compImg))
+}
+
+// exports.javascript = compJS
+// exports.estilos = compStyle
+// exports.imagens = compImg
